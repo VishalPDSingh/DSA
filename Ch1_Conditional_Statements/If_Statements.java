4bd724437a31e1f2ce52bd57f9c324a1619a0abe
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-11-06 10:54:48 +0530

    first commit

---
+++ Ch1_Conditional_Statements/If_Statements.java
@@ -0,0 +1,12 @@
+package Ch1_Conditional_Statements;
+
+public class If_Statements {
+    public static void main(String[] args) {
+        
+        int x = 5;
+        if(x == 5)
+        {
+            System.out.println("Number is 5");
+        }
+    }
+}
--- ch_Conditional_Statements/If_Statements.java
+++
@@ -1,7 +0,0 @@
-package ch_Conditional_Statements;
-
-public class If_Statements {
-    public static void main(String[] args) {
-        
-    }
-}
