8c5138a3a24355e5927beb69b75707c29ceec91b
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-04-04 12:19:20 +0530

    first commit

---
+++ ch10_NumberSystem/BitManipulation/ANDOperator.java
@@ -0,0 +1,14 @@
+package ch10_NumberSystem.BitManipulation;
+
+public class ANDOperator {
+    public static void main(String[] args) {
+        
+        int x = 5;
+        int y = 4;
+        System.out.println(x&y);
+        
+        System.out.println(23&10);
+
+        System.out.println(20&10);
+    }
+}
--- ch10_NumberSystem/BitManipulation/DemoBit01.java
+++
@@ -1,8 +0,0 @@
-package ch10_NumberSystem.BitManipulation;
-
-public class DemoBit01 {
-    public static void main(String[] args) {
-        
-        
-    }
-}
