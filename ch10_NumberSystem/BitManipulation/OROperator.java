04a3b276453de2e2a1877082ca42459694250395
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-04-06 20:34:38 +0530

    first commit

---
+++ ch10_NumberSystem/BitManipulation/OROperator.java
@@ -0,0 +1,8 @@
+package ch10_NumberSystem.BitManipulation;
+
+public class OROperator {
+    public static void main(String[] args) {
+        
+        System.out.println(23|10);
+    }
+}
---
+++ ch10_NumberSystem/BitManipulation/XOR_Operator.java
@@ -0,0 +1,8 @@
+package ch10_NumberSystem.BitManipulation;
+
+public class XOR_Operator {
+    public static void main(String[] args) {
+        
+        System.out.println(23^10);
+    }
+}
