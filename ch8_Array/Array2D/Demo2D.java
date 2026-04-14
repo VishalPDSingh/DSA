19f81e84db5274631d7254bfb3f00b31d635af42
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-04-14 06:15:36 +0530

    first commit

---
+++ ch8_Array/Array2D/Demo2D.java
@@ -0,0 +1,22 @@
+package ch8_Array.Array2D;
+
+public class Demo2D {
+    public static void main(String[] args) {
+
+        int arr[][];// Declaration
+        arr = new int[4][5]; // Initialization
+
+        // print the rowwise // Direct Initialization
+        int mat[][] = { { 10, 20, 30, 40, 50 },
+                { 60, 70, 80, 90, 100 },
+                { 110, 120, 130, 140, 150 },
+                { 160, 170, 180, 190, 200 } };
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print("(" + mat[i][j] + ")");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ ch8_Array/Array2D/LengthProperty.java
@@ -0,0 +1,10 @@
+package ch8_Array.Array2D;
+
+public class LengthProperty {
+    public static void main(String[] args) {
+        
+        int arr[][] = new int[5][4];
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+    }
+}
