e3e25a2c2c7769f737f843547e8b0ceadf587292
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-04-22 19:12:14 +0530

    first commit

--- ch13_SortingAlgorithm/BubbleSort.java
+++ ch13_SortingAlgorithm/BubbleSort.java
@@ -3,7 +3,6 @@ package ch13_SortingAlgorithm;
 public class BubbleSort {
     static void bubbleSort(int arr[], int n) {
         for (int pass = 0; pass <= n - 1; pass++) {
-            System.out.println(arr[pass]);
             for (int j = 1; j <= n - 1; j++) {
                 if (arr[j - 1] > arr[j]) {
                     int temp = arr[j - 1];
