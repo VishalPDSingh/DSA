0834ec9121ae8893af832719158b271e826ffa30
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-04-22 20:19:38 +0530

    first commit

--- ch9_String/chp_String/Demo1.java
+++ ch9_String/chp_String/Demo1.java
@@ -4,12 +4,14 @@ public class Demo1 {
     public static void main(String[] args) {
         // Method 1 to Create the string
         String s = new String("Java");
+        System.out.println(s);
         // method 2
         String s1 = "Java";
+        System.out.println(s1);
         // method 3 char to String 
         char[]c = {'a','b','c','d'};
         String s2 = new String(c);
-
+        System.out.println(s2);
         // Different way to Compare String
         
     }
