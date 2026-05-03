33c03a851836d55a124db6a26db24ccca0089548
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-05-03 20:54:15 +0530

    first commit

--- ch8_Array/Easy/Q12_LargestAltitude.java
+++ ch8_Array/Easy/Q12_LargestAltitude.java
@@ -1,26 +1,24 @@
 package ch8_Array.Easy;
 
 public class Q12_LargestAltitude {
-    static int largestAltitude(int gain[])
-    {
+    static int largestAltitude(int gain[]) {
         // starting point of biker
         int startPoint = 0;
         int maxHeight = 0;
 
-        for(int i=0; i<gain.length; i++)
-        {
-            startPoint = startPoint+gain[i];
-            if(startPoint > maxHeight)
-            {
+        for (int i = 0; i < gain.length; i++) {
+            startPoint = startPoint + gain[i];
+            if (startPoint > maxHeight) {
                 maxHeight = startPoint;
             }
         }
 
         return maxHeight;
     }
+
     public static void main(String[] args) {
-        
-        int gain[] = {-7, 5, 4, 3};
+
+        int gain[] = { -7, 5, 4, 3 };
         int res = largestAltitude(gain);
         System.out.println(res);
     }
