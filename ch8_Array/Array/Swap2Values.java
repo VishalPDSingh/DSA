f36a5e0cb51a9c2468d3e2d5f480fea09656c7eb
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-05-05 10:08:02 +0530

    first commit

---
+++ ch8_Array/Array/Swap2Values.java
@@ -0,0 +1,14 @@
+package ch8_Array.Array;
+
+public class Swap2Values {
+    public static void main(String[] args) {
+        
+        int a = 10; 
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
