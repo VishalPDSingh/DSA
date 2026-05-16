35038feeba4f83988a4793a919ec2fa1f0df4dbd
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-05-16 11:45:39 +0530

    first commit

---
+++ ch17_LinkedList/Demo1.java
@@ -0,0 +1,13 @@
+package ch17_LinkedList;
+/*
+class --> Blue Print
+object --> Instances of the blueprint
+
+Nested Class
+
+*/
+public class Demo1 {
+    public static void main(String[] args) {
+        
+    }
+}
