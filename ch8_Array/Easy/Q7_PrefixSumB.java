9f0b4d7676f5efa3ebd4892970f92b351dfcbe17
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-06-01 12:28:45 +0530

    firstCommit

---
+++ ch8_Array/Easy/Q7_PrefixSumB.java
@@ -0,0 +1,23 @@
+package ch8_Array.Easy;
+
+public class Q7_PrefixSumB {
+    static int[] prefixSum(int arr[]) {
+        int pf[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j <= i; j++) {
+                sum = sum + arr[j];
+            }
+            pf[i] = sum;
+        }
+        return pf;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 1, 6, -2, 7 };
+        int res[] = prefixSum(arr);
+        for (int ans : res) {
+            System.out.println(ans);
+        }
+    }
+}
